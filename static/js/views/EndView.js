65998671777ece009e39bf16a4c91b7d8f4975fc
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2023-04-04 13:23:37 +0200

    a

--- static/js/views/EndView.js
+++ static/js/views/EndView.js
@@ -10,13 +10,13 @@ export default class extends AbstractView {
     return `
     <div class="manualContainerEnd">
         <div class="CloseButtonContainer">
-          <a href="/" class="closeButton" data-link><img src="/static/images/Exit.png"
+          <a href="/mainMenu" class="closeButton" data-link><img src="/static/images/Exit.png"
           height = "24px" width = "24px" ></a>
         </div>
         <img class="headerEnd imageEnd" src="/static/images/Schatzinsel_Überschrift.png" width = "500px">
         <img class="imageEnd" src="/static/images/Schatz_geborgen.png" width = "500px">
         <img src="/static/images/Herzlichen_Glückwunsch.png" width = "500px">
-        <a href="/" class="button" data-link>Zurück zum Hauptmenü!</a>
+        <a href="/mainMenu" class="button" data-link>Zurück zum Hauptmenü!</a>
     </div>
     `;
   }
